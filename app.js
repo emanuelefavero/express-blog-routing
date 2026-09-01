e5d5f1007dfda117b17b902f6ba774460fa32067
Emanuele Favero <emafaux89@gmail.com>
2026-09-02 00:51:25 +0200

    feat: handle not found endpoints

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ import path from 'node:path';
 import express from 'express';
 import { postsRouter } from './routers/posts.js';
 
-const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+const app = express();
 
 app.use(express.static(path.join(import.meta.dirname, 'public')));
 
 app.get('/', (_, res) => res.json({ message: 'Visita /posts' }));
 
 app.use('/posts', postsRouter);
+
+app.use((_, res) => res.status(404).json({ message: 'Not Found' })); // 404
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
